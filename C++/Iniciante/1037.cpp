04ef63fd04cba710ce8f5ae02c83826a7ff8df44
Raphael Sander <raphael.sander75@gmail.com>
2018-02-26 19:27:44 -0300

    Create 1037.cpp

---
+++ C++/Iniciante/1037.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+  
+using namespace std;
+  
+int main(){ 
+    //Variáveis
+    double n;
+      
+    //Entrada de dados
+    cin >> n;
+      
+    //Saída de dados
+    
+    if((n>=0) && (n<=25)){
+       cout << "Intervalo [0,25]" << endl;
+    }else if((n>25) && (n<=50)){
+       cout << "Intervalo (25,50]" << endl;
+    }else if((n>50) && (n<=75)){
+       cout << "Intervalo (50,75]" << endl;
+    }else if((n>75) && (n<=100)){
+       cout << "Intervalo (75,100]" << endl;
+    }else{
+       cout << "Fora de intervalo" << endl;
+    }
+    system("pause");
+    return 0;
+}
