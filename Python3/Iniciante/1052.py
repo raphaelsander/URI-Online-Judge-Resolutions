7e9cc029219328e4972ae87a4ac745624606bf4b
Raphael Sander <raphael.sander75@gmail.com>
2018-04-26 15:08:56 -0300

    Create 1052.py

---
+++ Python3/Iniciante/1052.py
@@ -0,0 +1,7 @@
+# -*- encoding: utf-8 -*-
+
+X = int(input())
+
+meses = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+print("%s" % (meses[X-1]))
